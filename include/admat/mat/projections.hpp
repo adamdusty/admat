9f54a28db33cba2a0031fb3660c7f18565750171
Adam Dusty <adam.dusty27@gmail.com>
2024-01-03 16:20:55 -0700

    Fixed another erroneously constexpr function

--- include/admat/mat/projections.hpp
+++ include/admat/mat/projections.hpp
@@ -2,41 +2,12 @@
 
 #include "admat/mat/mat.hpp"
 #include "admat/vec.hpp"
-#include <adizzle/assert.hpp>
-#include <cmath>
-#include <numbers>
 
 namespace admat {
 
 // Not eligible for constexpr due to sqrt in func dependency
 auto look_at(const vec3& position, const vec3& target, const vec3& up) -> mat4;
-
-// windows.h includes files that define 'near' and 'far' as macros, which causes this function to error
-// at compile time with difficult to understand error messages.
-#pragma push_macro("near")
-#pragma push_macro("far")
-#undef near
-#undef far
-
-constexpr auto perspective(float fov, float aspect, float near, float far) -> mat4 {
-    ADIZZLE_ASSERT(fov > 0.0f && fov < static_cast<float>(std::numbers::pi), "Invalid FOV");
-    ADIZZLE_ASSERT(near > 0.0f, "Near plane distance less than zero.");
-    ADIZZLE_ASSERT(far > near, "Far plane distance less than zero.");
-    ADIZZLE_ASSERT(far > 0.0f, "Far plane distance less than near plane.");
-
-    float y_scale = 1.0f / std::tan(fov * 0.5f);
-    float x_scale = y_scale / aspect;
-
-    auto mat = mat4{};
-
-    mat(0, 0) = x_scale;
-    mat(1, 1) = y_scale;
-    mat(2, 2) = far / (near - far);
-    mat(2, 3) = -1.0f;
-    mat(3, 2) = near * far / (near - far);
-
-    return mat;
-}
+auto perspective(float fov, float aspect, float near_plane, float far_plane) -> mat4;
 
 constexpr auto orthographic(float width, float height, float near, float far) -> mat4 {
     float range = 1.0f / (near - far);
@@ -51,8 +22,5 @@ constexpr auto orthographic(float width, float height, float near, float far) ->
 
     return mat;
 }
-
-#pragma pop_macro("far")
-#pragma pop_macro("near")
 
 } // namespace admat
--- src/mat/projections.cpp
+++ src/mat/projections.cpp
@@ -1,5 +1,9 @@
 #include "admat/mat/projections.hpp"
 
+#include <adizzle/assert.hpp>
+#include <cmath>
+#include <numbers>
+
 namespace admat {
 
 auto look_at(const vec3& position, const vec3& target, const vec3& up) -> mat4 {
@@ -16,6 +20,26 @@ auto look_at(const vec3& position, const vec3& target, const vec3& up) -> mat4 {
     mat.set_row(1, {x.y, y.y, z.y, 0.0f});
     mat.set_row(2, {x.z, y.z, z.z, 0.0f});
     mat.set_row(3, {dot(x, neg_pos), dot(y, neg_pos), dot(z, neg_pos), 1.0f});
+
+    return mat;
+}
+
+auto perspective(float fov, float aspect, float near_plane, float far_plane) -> mat4 {
+    ADIZZLE_ASSERT(fov > 0.0f && fov < static_cast<float>(std::numbers::pi), "Invalid FOV");
+    ADIZZLE_ASSERT(near_plane > 0.0f, "Near plane distance less than zero.");
+    ADIZZLE_ASSERT(far_plane > near_plane, "Far plane distance less than zero.");
+    ADIZZLE_ASSERT(far_plane > 0.0f, "Far plane distance less than near plane.");
+
+    float y_scale = 1.0f / std::tan(fov * 0.5f);
+    float x_scale = y_scale / aspect;
+
+    auto mat = mat4{};
+
+    mat(0, 0) = x_scale;
+    mat(1, 1) = y_scale;
+    mat(2, 2) = far_plane / (near_plane - far_plane);
+    mat(2, 3) = -1.0f;
+    mat(3, 2) = near_plane * far_plane / (near_plane - far_plane);
 
     return mat;
 }
