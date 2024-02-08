b183a5406952236d7a729353aa9ac02cd2b3ffd0
Adam Dusty <adam.dusty27@gmail.com>
2024-02-07 20:59:48 -0700

    Updated cmake to play nicer with other projects

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,12 +1,15 @@
 include(FetchContent)
 
+if(NOT TARGET adizzle::adizzle)
+
 FetchContent_Declare(
     adizzle
     GIT_REPOSITORY https://github.com/adamdusty/adizzle.git
-    GIT_TAG        origin/main
+    GIT_TAG        main
     GIT_SHALLOW    TRUE
     SYSTEM
     FIND_PACKAGE_ARGS
 )
-
 FetchContent_MakeAvailable(adizzle)
+
+endif()
--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,2 +1,1 @@
-option(ADMAT_BUILD_TESTS "Build tests for admat" OFF)
 option(ADMAT_BUILD_BENCH "Build benchmarks for admat" OFF)
