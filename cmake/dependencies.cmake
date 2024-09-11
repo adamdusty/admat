289c708b1e0be68cb3eb634a65b335ed86e33786
Adam Dusty <adam.dusty27@gmail.com>
2024-09-11 10:06:56 -0700

    Add libassert

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,1 +1,10 @@
 include(FetchContent)
+
+FetchContent_Declare(
+  libassert
+  GIT_REPOSITORY https://github.com/jeremy-rifkin/libassert.git
+  GIT_TAG        v2.1.0
+  SYSTEM
+)
+
+FetchContent_MakeAvailable(libassert)
